a8f1a8e4b130831a89cb8596a785a9517f6a22ea
THCKwarter <3734237+THCKwarter@users.noreply.github.com>
2018-03-17 21:34:21 -0400

    Added Ch5 Bonus + Renamed Ch4 Folder

---
+++ Chapter5Bonus/HopTable.java
@@ -0,0 +1,6 @@
+//CSCI3200 - Chapter 5 Bonus - Hopscotch Hash Table
+//Matthew Johnston
+
+public class HopTable {
+
+}
---
+++ Chapter5Bonus/HopTableDriver.java
@@ -0,0 +1,9 @@
+//CSCI3200 - Chapter 5 Bonus - Hopscotch Hash Table Driver
+//Matthew Johnston
+
+public class HopTableDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
