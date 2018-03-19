b9b3f709668c2fedacfd652617124d4524facd99
THCKwarter <3734237+THCKwarter@users.noreply.github.com>
2018-03-18 20:58:50 -0400

    Updated Ch5 Bonus (WIP)

--- Chapter5Bonus/HopTable.java
+++ Chapter5Bonus/HopTable.java
@@ -6,6 +6,7 @@ public class HopTable<E>{
 	private int tableSize;
 	private int hopRange;
 	private int occupied;
+	private int totalSize;
 	private Entry<E>[] array;
 	
 	//Constructors
@@ -23,6 +24,47 @@ public class HopTable<E>{
 		tableSize = size;
 		hopRange = hops;
 		createArray(tableSize);
+	}
+	
+	//Insert
+	public boolean insert(E e){
+		if(!isEmpty()){
+			return false;
+		}
+		
+		int currentPos = findPos(e);
+		
+		if(currentPos == -1){
+			return false;
+		}
+		
+		if(array[currentPos] != null){
+			e = array[currentPos].element;
+			array[currentPos].isActive = true;
+		}
+		
+		String hope;
+		if(array[currentPos] != null){
+			hope = array[currentPos].hope;
+			e = array[currentPos].element;
+		}else{
+			hope = "10000000";
+		}
+		
+		array[currentPos] = new Entry<>(e, hope, true);
+		totalSize++;
+		
+		return true;
+	}
+	
+	private int myHash(E e){
+		int hashVal = e.hashCode();
+		
+		hashVal %= array.length;
+		if(hashVal < 0){
+			hashVal += array.length;
+		}
+		return hashVal;
 	}
 	
 	//Create array
