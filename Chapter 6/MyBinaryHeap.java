e9140ed727a4f0838ea7626337396845baf4bfeb
THCKwarter <3734237+THCKwarter@users.noreply.github.com>
2018-03-27 22:05:46 -0400

    Finished Algorithm 6A

--- Chapter 6/MyBinaryHeap.java
+++ Chapter 6/MyBinaryHeap.java
@@ -37,6 +37,23 @@ public class MyBinaryHeap<E extends Comparable<? super E>> {
 		//fix heap structure
 		buildHeap();//add operation counts to this method
 	}
+	public E getKth(int k){
+		E kth = null;
+		int count = k;
+		System.out.println("Count: " + count);
+		
+		while(count != 0){
+			kth = this.deleteMin();
+			count--;
+		}
+			
+		if(kth == null){
+			System.out.println("Kth value was out of range.");
+			return null;
+		}else{
+			return kth;
+		}
+	}
 	public int getOpCount(){
 		return operationCount;
 	}
@@ -45,21 +62,29 @@ public class MyBinaryHeap<E extends Comparable<? super E>> {
 	{
 		String output = "Size:" + currentSize + "\n";
 
-		//output += Arrays.toString(heap);//for testing
-
 		//only print valid values
-		for(int i = 1; i <= currentSize; i++)
-			output += heap[i] +", ";
+		output += "[";
+		for(int i = 1; i <= currentSize; i++) {
+			if(heap[i+1] == null){
+				output += heap[i] + "]";
+			}else{
+				output += heap[i] + ", ";
+			}
+		}
 
 		return output;
 	}
 	public void buildHeap()
 	{
+		int outsideCount = (currentSize/2);
+		int perCount = 0;
 		//start with lowest parent
+		operationCount++;
 		for(int i = currentSize / 2; i > 0; i--)
 		{
-			percolateDown(i);
+			perCount += percolateDown(i);
 		}
+		operationCount += (outsideCount*perCount);
 	}
 
 	public void insert(E item)
@@ -115,26 +140,43 @@ public class MyBinaryHeap<E extends Comparable<? super E>> {
 		//return smallest item
 		return temp;
 	}
-	private void percolateDown(int pos)
+	private int percolateDown(int pos)
 	{
+		int perDownOpCount = 0;
+		perDownOpCount++;
 		int child;
+		perDownOpCount++;
 		E temp = heap[pos];
+		
+		int outsideCount = pos;
+		int insideCount = 0;
 		//check if there are children
 		for(; pos*2 <= currentSize; pos = child)
 		{
+			insideCount++;
 			child = pos*2;
 			//is there 2 children
 			//if there are check if second child is smaller
-			if(child != currentSize &&
-					heap[child+1].compareTo(heap[child]) < 0)
+			insideCount++;
+			if(child != currentSize && heap[child+1].compareTo(heap[child]) < 0) {
 				child++;
+				insideCount++;
+			}
+				
 			//smaller child compare to parent
-			if(heap[child].compareTo(temp) < 0)
+			insideCount++;
+			if(heap[child].compareTo(temp) < 0){
+				insideCount++;
 				heap[pos] = heap[child];
-			else
+			}else{
 				break;
+			}
 		}
+		perDownOpCount++;
 		heap[pos] = temp;
+		
+		perDownOpCount += (outsideCount*insideCount);
+		return perDownOpCount;
 	}
 	public E findMin()
 	{
